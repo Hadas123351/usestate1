c95a2a6b3712b327fd1d2a8c2c3931d83b10806f
hadas saidyan <hadas.saidyan@gmail.com>
2022-11-21 10:46:49 +0200

    Counter

---
+++ src/Counter.js
@@ -0,0 +1,12 @@
+function Counter() {
+    const count = 1;
+    return (
+      <div>
+        <h1>Counter</h1>
+        <p>Counter is at {count}</p>
+        <button>Click to add 1 to Counter</button>
+      </div>
+    );
+  }
+  
+  export default Counter;

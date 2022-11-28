15d9d3e25ab79f769e1626e6bd223dd37f2afb11
hadas saidyan <hadas.saidyan@gmail.com>
2022-11-28 23:56:53 +0200

    Exercise 4 APP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 
 import './App.css';
 import Counter from './Counter';
+import { useState } from 'react';
 
 function App() {
+  const [delta, setDelta] = useState(1)
+  const [numMax, setNumMax] = useState(1)
+
+function handleDelta(e){
+  console.log(e);
+  setDelta(Number(e.target.value))
+}
+function handleNumMax(e){
+  console.log(e);
+  setNumMax(Number(e.target.value))
+}
+
   return (
     <div className="App">
-      <Counter/>
+      <p><h3>Couner number:</h3></p>
+      <input type = "number" value = {delta} onChange = {handleDelta}/>
+      <p><h3>Enter Max number:</h3></p>
+      <input type = "number" value = {numMax} onChange = {handleNumMax}/>
+      <Counter delta = {delta} numMax = {numMax}/>
+      <Counter delta = {delta} numMax = {numMax} />
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,19 @@
 import { useState } from "react";
-function Counter() {
-    const [count, setCount] = useState(1)
-    const [delta, setDelta] = useState(1)
+function Counter(props) {
+    const {delta} = props
+    const {numMax} = props
+    const [count, setCount] = useState(0)
+
     function incr(){
         setCount(
             function(oldcount){
-                return oldcount + delta
-            }
+                    if((oldcount + delta) < numMax) {
+                        return (oldcount + delta)
+                    }
+                    else {
+                        return 0
+                    }
+                }
         )
         console.log(count)
     }
@@ -14,15 +21,10 @@ function Counter() {
     function reset(){
         setCount(0)
     }
-    function handleDelta(e){
-        console.log(e);
-        setDelta(Number(e.target.value))
-    }
 
     return (
       <div>
         <h1>Counter</h1>
-        <input type = "number" value = {delta} onChange = {handleDelta}/>
         <p>Counter is at {count}</p>
         <button onClick={incr}>Click to add {delta} to Counter</button>
         <button onClick={reset}> reset Counter</button>
